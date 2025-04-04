18d9c1dfb53d95e550e148d396e6566801798435
miznaansari <miznaansari@gmail.com>
2025-04-04 18:43:11 +0530

    Update

--- src/data/songs.js
+++ src/data/songs.js
@@ -100,9 +100,3 @@ const songs = [
 export default songs;
 
 
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/miznaansari/clonespotify.git
-git push -u origin main
